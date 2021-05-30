530fee237ca96310adb4dcff405bc0006b6fa519
frans <fransmbm@gmail.com>
2021-05-30 13:56:29 +0700

    ini gaes login dan register nya cuman aku baru bisa buat sampe login nya aja itu.

---
+++ tubes_pwl/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Selamat Program Login Anda Berhadil</h1>
+<ul class="nav nav-treeview">
+  <li class="nav-item">
+    <a href="{{ route('logout')}}" class="nav-link">
+      <i class="nav-icon fas fa-th"></i>
+      <p>
+        Logout
+      </p>
+    </a>
+  </li>
+</ul>
+
+</body>
+</html>
